8ba0de737800a06ea70d1f4a0166868b76a640c5
Furbixtubs <furbixdev@gmail.com>
2026-03-24 01:40:08 +0100

    new console

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
 async function request(endpoint, options = {}) {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
     credentials: "include",
